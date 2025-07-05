40d03c2baf02a79cad0a5742c94c2d3c1a281fbf
mulutu <mulutujackson@gmail.com>
2025-07-05 23:39:18 +0300

    fix: update gRPC protobuf compatibility to SupportPackageIsVersion6

--- internal/proto/events_grpc.pb.go
+++ internal/proto/events_grpc.pb.go
@@ -13,7 +13,7 @@ import (
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion7
+const _ = grpc.SupportPackageIsVersion6
 
 const (
 	AgentIngest_Authenticate_FullMethodName    = "/proto.AgentIngest/Authenticate"
